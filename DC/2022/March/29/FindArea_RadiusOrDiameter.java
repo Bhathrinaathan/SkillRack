197d7e785a651582cca8987ed6899de90528ed57
Bhathrinaathan M B <75372249+Bhathrinaathan@users.noreply.github.com>
2022-03-30 16:22:55 +0530

    Create FindArea_RadiusOrDiameter.java

---
+++ DC/2022/March/29/FindArea_RadiusOrDiameter.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+class Circle {
+
+    private double radius;
+
+    public Circle(int radius) {
+        this.radius = radius;
+    }
+    public Circle(double diameter)
+    {
+        radius=diameter/2.0;
+    }
+    double getArea()
+    {
+        return ((22/7.0)*radius*radius);
+    }
+} // end of Circle class
+
+public class Hello {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int Q = Integer.parseInt(sc.nextLine().trim());
+        Circle circle = null;
+        if (Q == 1) {
+            int radius = Integer.parseInt(sc.nextLine().trim());
+            circle = new Circle(radius);
+        } else if (Q == 2) {
+            double diameter = Double.parseDouble(sc.nextLine().trim());
+            circle = new Circle(diameter);
+        }
+        System.out.print(String.format("%.2f", circle.getArea()));
+    } // end of main function
+} // end of Hello class
